e268de1e7ae06a7b9ab9d59411959e61680001cd
tchi-sama <tchisama41@gmail.com>
2023-02-18 14:39:28 -0500

    setup for firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxBOxslDl1p8emkyQD5TOwI4Mj7-HFHSM",
+  authDomain: "learn-firebase-with-ninja.firebaseapp.com",
+  projectId: "learn-firebase-with-ninja",
+  storageBucket: "learn-firebase-with-ninja.appspot.com",
+  messagingSenderId: "1034556543731",
+  appId: "1:1034556543731:web:0ec7d1800d3f45b075dd86"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
